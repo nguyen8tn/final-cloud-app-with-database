fdf52f1ed4dc7553f451a74906dab8d4e76027c9
Your Name <you@example.com>
2023-08-09 08:21:57 -0400

    first commit

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -115,14 +115,35 @@ class Enrollment(models.Model):
     #    else:
     #        return False
 
-
+class Question(models.Model):
+    # Foreign key to Course
+    course = models.ForeignKey(Course, on_delete = models.CASCADE)
+    # question text
+    title = models.TextField()
+    # question grade/mark
+    grade = models.IntegerField(default = 0)
+    
+    # <HINT> A sample model method to calculate if learner get the score of the question
+    def is_get_score(self, selected_ids):
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+        if all_answers == selected_correct:
+            return True
+        else:
+            return False
+    
 #  <HINT> Create a Choice Model with:
     # Used to persist choice content for a question
     # One-To-Many (or Many-To-Many if you want to reuse choices) relationship with Question
     # Choice content
     # Indicate if this choice of the question is a correct one or not
     # Other fields and methods you would like to design
+
 # class Choice(models.Model):
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete= models.CASCADE)
+    text  = models.TextField()
+    is_correct = models.BooleanField(default = False)
 
 # <HINT> The submission model
 # One enrollment could have multiple submission
@@ -132,3 +153,7 @@ class Enrollment(models.Model):
 #    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
 #    choices = models.ManyToManyField(Choice)
 #    Other fields and methods you would like to design
+
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
